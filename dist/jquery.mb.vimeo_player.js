c5cabe13e736db9516e9b76672669e239eef9c46
Matteo Bicocchi <matbicoc@gmail.com>
2018-02-28 14:20:42 +0100

    Release v1.1.7 stable

--- dist/jquery.mb.vimeo_player.js
+++ dist/jquery.mb.vimeo_player.js
@@ -37,8 +37,8 @@ var get_vimeo_videoID = function( url ) {
   jQuery.vimeo_player = {
     name: "jquery.mb.vimeo_player",
     author: "Matteo Bicocchi (pupunzi)",
-    version: "1.1.6",
-    build: "490",
+    version: "1.1.7",
+    build: "491",
     defaults: {
       containment: "body",
       ratio: 16/9, // "16/9" or "4/3"
